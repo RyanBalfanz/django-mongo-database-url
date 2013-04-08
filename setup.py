e83f1d07ca6adff14f28b0e4bbc3364f590b19c8
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-04-08 16:52:00 +0530

    Bumped version to v0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
 	name='dj-mongo-database-url',
-	version='0.1',
+	version='0.1.1',
 	url='https://github.com/RyanBalfanz/django-mongo-database-url',
 	# license='BSD',
 	author='Ryan Balfanz',
